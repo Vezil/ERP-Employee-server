93a8b03547c96f5a1ba7d34aaaeffdff78e68939
Vezil <SzymonWojaczek@wp.pl>
2021-12-04 17:57:08 +0100

    add userId cascade on model

--- models/Contracts.js
+++ models/Contracts.js
@@ -6,7 +6,16 @@ module.exports = (sequelize, DataTypes) => {
         {
             contract_length: DataTypes.INTEGER,
             start_date: DataTypes.DATEONLY,
-            finish_date: DataTypes.DATEONLY
+            finish_date: DataTypes.DATEONLY,
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                onDelete: 'cascade',
+                references: {
+                    model: 'User',
+                    key: 'id'
+                }
+            }
         },
         {
             tableName: 'contracts',
